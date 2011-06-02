72251cb0902b141eacdf700f05bbeca470835358
jake <bigjake2s@gmail.com>
2011-06-02 03:33:01 +0000

    Auto-creating databases in the webroot if they don't exist.

--- webroot.conf.php
+++ webroot.conf.php
@@ -9,6 +9,9 @@ switch (DATABASE) {
 		$config['database.user'] = 'root';
 		$config['database.pass'] = '';
 		$config['database.name'] = 'test';
+		mysql_connect($config['database.host'], $config['database.user'], $config['database.pass']);
+		mysql_query("CREATE DATABASE IF NOT EXISTS " . $config['database.name']);
+		mysql_close();
 		break;
 	case 'master/slave':
 		$config['database.master_host'] = 'localhost';
@@ -18,6 +21,9 @@ switch (DATABASE) {
 		$config['database.slave_user'] = 'root';
 		$config['database.slave_pass'] = '';
 		$config['database.name'] = 'test';
+		mysql_connect($config['database.master_host'], $config['database.master_user'], $config['database.master_pass']);
+		mysql_query("CREATE DATABASE IF NOT EXISTS " . $config['database.name']);
+		mysql_close();
 		break;
 	default:
 		$config['database.auto_connect'] = false;
@@ -27,6 +33,7 @@ switch (DATABASE) {
 $config['library.dir'] = dirname(dirname(__FILE__));
 $config['cache.dir'] = '/tmp/framework-cache';
 
+// Make anything that might be missing
 if (!is_dir($config['cache.dir'])) {
 	mkdir($config['cache.dir']);
 }
