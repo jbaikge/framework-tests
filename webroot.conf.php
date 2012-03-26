174a9326304649a57624fbb1f32f8e98401d985b
jake <bigjake2s@gmail.com>
2012-03-26 15:10:36 +0000

    tests/webroot - Cranked error_reporting to 11 if it wasn't already, added reporting based on environment variables

--- webroot.conf.php
+++ webroot.conf.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL | E_STRICT);
+
 if (!defined('DATABASE')) {
 	define('DATABASE', null);
 }
@@ -31,7 +33,7 @@ if (array_key_exists('database.auto_connect', $config) && $config['database.auto
 		mysql_connect($config['database.master_host'], $config['database.master_user'], $config['database.master_pass']);
 	}
 	mysql_query("CREATE DATABASE IF NOT EXISTS " . $config['database.name']);
-	mysql_close();	
+	mysql_close();
 }
 
 $config['library.dir'] = dirname(dirname(__FILE__));
@@ -40,6 +42,8 @@ $config['templates.form.dir'] = $config['library.dir'] . '/form/templates';
 $config['templates.form.field.dir'] = $config['library.dir'] . '/form/field/templates';
 $config['templates.calendar.dir'] = $config['library.dir'] . '/calendar/templates';
 $config['class_filter.excluded'] = array('.svn' => true);
+$config['report.enabled'] = isset($_SERVER['REPORTS_ENABLED']) ? $_SERVER['REPORTS_ENABLED'] : false;
+$config['report.frequency'] = isset($_SERVER['REPORT_FREQUENCY']) ? $_SERVER['REPORT_FREQUENCY'] : 0;
 
 // Make anything that might be missing
 if (!is_dir($config['cache.dir'])) {
